c04400c7ea2f080812a664c4d815b59a90d3bedd
Akyla <akylaaquino@hotmail.com>
2025-01-31 16:41:00 -0300

    feat: Feature engineering and treating categoric columns

--- notebooks/utils/exploratory_data_utils.py
+++ notebooks/utils/exploratory_data_utils.py
@@ -3,3 +3,10 @@ import pandas as pd
 
 def remove_columns(df: pd.DataFrame, columns: list) -> pd.DataFrame:
     return df.drop(columns=columns)
+
+
+def get_bairro_mean_price(df: pd.DataFrame, bairro: list) -> pd.DataFrame:
+    for b in bairro:
+        mean_price = df[df["bairro"] == b]["price"].mean()
+        df.loc[df["bairro"] == b, 'media_bairro'] = mean_price
+    return df
